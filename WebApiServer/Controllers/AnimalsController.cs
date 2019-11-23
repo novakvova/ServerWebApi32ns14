d657ea99520431563008af973f37dfb417539406
novakvova <novakvova@gmail.com>
2019-11-23 16:21:51 +0200

    Create base AnimalController

---
+++ WebApiServer/Controllers/AnimalsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiServer.DTO;
+
+namespace WebApiServer.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    //[ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        // api/animals
+        [HttpGet]
+        public IActionResult GetAnimals()
+        {
+            List<AnimalDTO> model = new List<AnimalDTO>();
+            model.Add(new AnimalDTO
+            {
+                Name="Боря",
+                Bread="Манул",
+                DateBirdth="12.12.2018",
+                Image= "https://dinoanimals.com/wp-content/uploads/2016/02/Pallass_cat.jpg"
+            });
+
+            model.Add(new AnimalDTO
+            {
+                Name = "Оксана",
+                Bread = "Руский голубий",
+                DateBirdth = "07.01.2017",
+                Image = "https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2018/06/cat-217679.jpg?h=c4ed616d&itok=3qHaqQ56"
+            });
+            return Ok(model);
+        }
+    }
+}
---
+++ WebApiServer/DTO/AnimalDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiServer.DTO
+{
+    public class AnimalDTO
+    {
+        public string Name { get; set; }
+        public string Bread { get; set; }
+        public string Image { get; set; }
+        public string DateBirdth { get; set; }
+    }
+}
